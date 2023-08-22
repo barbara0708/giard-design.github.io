5c92852af8f3f39a131fa7b4631f3ec0fe0e9d9c
barbara <marcelineabadir527@gmail.com>
2023-08-22 12:58:05 +0200

    images

--- src/index.js
+++ src/index.js
@@ -52,6 +52,23 @@ var macyInstance = Macy({
         macyInstance.recalculate();
       }  
         
-    });
-    
+    });    
   }
+
+
+    $('.popup-image').magnificPopup({
+      type: 'image',
+      mainClass: 'mfp-with-zoom', 
+      gallery:{
+          enabled:true
+        },
+    
+      zoom: {
+        enabled: true, 
+        duration: 300, 
+        easing: 'ease-in-out', 
+        opener: function(openerElement) {
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    }
+    });
